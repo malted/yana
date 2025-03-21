522649e44d5057e0d0990ebe38f353c07925fa83
Malted <malted@malted.dev>
2025-03-21 17:00:42 -0400

    Initial Commit

---
+++ Yana/YanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  YanaApp.swift
+//  Yana
+//
+//  Created by Ben Dixon on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct YanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
